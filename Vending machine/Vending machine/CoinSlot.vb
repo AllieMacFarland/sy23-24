f2fa390d36eb016385ea929e915da06c795cdfb0
Allie <zamac956@learner.dvusd.org>
2024-02-08 08:18:41 -0700

    update

--- Vending machine/Vending machine/CoinSlot.vb
+++ Vending machine/Vending machine/CoinSlot.vb
@@ -1,6 +1,5 @@
-﻿Imports System.Runtime.InteropServices
-
-Public Class CoinSlot
+﻿Public Class CoinSlot
+    Public Event CoinReturnEvent(D As Integer, Q As Integer, DM As Integer, N As Integer)
     Public Property Quarters As Integer
     Public Property Nickles As Integer
     Public Property Dimes As Integer
@@ -10,6 +9,13 @@ Public Class CoinSlot
             Return Dollars * 1 + Quarters * 0.25 + Dimes * 0.1 + Nickles * 0.05
         End Get
     End Property
+    Public Sub CoinReturn()
+        RaiseEvent CoinReturnEvent(Dollars, Quarters, Nickles, Dimes)
+        Quarters = 0
+        Dimes = 0
+        Nickles = 0
+        Dollars = 0
+    End Sub
     Public Sub InsertQuarter()
         Quarters = Quarters + 1
     End Sub
--- Vending machine/Vending machine/Form1.vb
+++ Vending machine/Vending machine/Form1.vb
@@ -1,23 +1,31 @@
 ﻿Public Class Form1
-    Dim CS As New CoinSlot
+    Dim WithEvents CS As New CoinSlot
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         CS.InsertQuarter()
-        Label1.Text = CS.Total
+        Label1.Text = CS.Total.ToString("C2")
     End Sub
-
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         CS.InsertDollar()
-        Label1.Text = CS.Total
+        Label1.Text = CS.Total.ToString("C2")
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         CS.InsertNickle()
-        Label1.Text = CS.Total
+        Label1.Text = CS.Total.ToString("C2")
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         CS.InsertDime()
-        Label1.Text = CS.Total
+        Label1.Text = CS.Total.ToString("C2")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        CS.CoinReturn()
+        Label1.Text = CS.Total.ToString("C2")
+    End Sub
+
+    Private Sub CS_CoinReturnEvent(D As Integer, Q As Integer, DM As Integer, N As Integer) Handles CS.CoinReturnEvent
+        BackColor = Color.Red
     End Sub
 End Class
