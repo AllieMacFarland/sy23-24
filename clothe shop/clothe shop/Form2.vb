e524fe691656c41ebae3737d6e29f1510d96185d
Allie <zamac956@learner.dvusd.org>
2024-03-28 08:05:29 -0700

    finished

--- clothe shop/clothe shop/Form2.vb
+++ clothe shop/clothe shop/Form2.vb
@@ -41,6 +41,142 @@
     End Sub
 
     Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button9.Image
+        F6.ShowDialog()
+    End Sub
 
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button10.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button23_Click(sender As Object, e As EventArgs) Handles Button23.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button23.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button24_Click(sender As Object, e As EventArgs) Handles Button24.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button24.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button11.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button12.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button13.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button14.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button15.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button16.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button25_Click(sender As Object, e As EventArgs) Handles Button25.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button25.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button26_Click(sender As Object, e As EventArgs) Handles Button26.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button26.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button17.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button18.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button19.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button20_Click(sender As Object, e As EventArgs) Handles Button20.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button20.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button21_Click(sender As Object, e As EventArgs) Handles Button21.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button21.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button22_Click(sender As Object, e As EventArgs) Handles Button22.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button22.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button27_Click(sender As Object, e As EventArgs) Handles Button27.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button27.Image
+        F6.ShowDialog()
+    End Sub
+
+    Private Sub Button28_Click(sender As Object, e As EventArgs) Handles Button28.Click
+        Dim F6 As New Form6
+        Me.Hide()
+        F6.ItemInfo1.picture = Button28.Image
+        F6.ShowDialog()
     End Sub
 End Class
